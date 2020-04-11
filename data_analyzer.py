88a5ad9638a9136f1eda3db5305649d22b58ecf8
Jack Xu <jackxxu@gmail.com>
2020-04-11 10:32:05 -0400

    adjust folder name

--- data_analyzer.py
+++ data_analyzer.py
@@ -4,5 +4,15 @@ import matplotlib.pyplot as plt
 
 import re
 import lzma, json
+import csv
 
+with lzma.open("New Mexico-20200302-xml/data/data.jsonl.xz") as in_file:
+    for line in in_file:
+        case = json.loads(str(line, 'utf8'))
+        cases.append(case)
 
+# with open('data/cases.csv', mode='w') as csvfile: 
+#     fieldnames = ['emp_name']
+#     writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
+#     writer.writerow({'emp_name': 'foo'})
+#     writer.writerow({'emp_name': 'bar'})
