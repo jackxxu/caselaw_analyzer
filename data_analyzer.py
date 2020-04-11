8f6c5c25adbfd2e36c3162f41d8d6fba72129768
Jack Xu <jackxxu@gmail.com>
2020-04-11 11:13:17 -0400

    initial parsing

--- data_analyzer.py
+++ data_analyzer.py
@@ -6,13 +6,47 @@ import re
 import lzma, json
 import csv
 
-with lzma.open("New Mexico-20200302-xml/data/data.jsonl.xz") as in_file:
-    for line in in_file:
-        case = json.loads(str(line, 'utf8'))
-        cases.append(case)
+with open('data/cases.csv', mode='w') as csvfile: 
+    fieldnames = ['id', 'url', 'name', 'name_abbreviation', 'decision_date', 'docket_number', 
+                  'first_page', 'last_page', 'frontend_url',
+                  'citations', 'citation_0_type', 'citation_0', 'citation_1_type', 'citation_1', 'citation_2_type', 'citation_2',
+                  'volume_barcode', 'volume_number',
+                  'reporter_name', "reporter_id",
+                  'court_id', 'court_name',
+                  'jurisdiction_id', 'jurisdiction_name']
+    output = csv.DictWriter(csvfile, fieldnames=fieldnames)
+    output.writeheader()
 
-# with open('data/cases.csv', mode='w') as csvfile: 
-#     fieldnames = ['emp_name']
-#     writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
-#     writer.writerow({'emp_name': 'foo'})
-#     writer.writerow({'emp_name': 'bar'})
+    with lzma.open("New Mexico-20200302-xml/data/data.jsonl.xz", mode='r') as in_file:
+        for line in in_file:
+            case = json.loads(str(line, 'utf8'))
+
+            for i, citation in enumerate(case['citations']): 
+                if i > 3: 
+                    print(f"more than {len(case['citations'])} citations")
+                    break
+                case[f'citation_{i}_type'] = citation['type']
+                case[f'citation_{i}'] = citation['cite']
+            case['citations'] = len(case['citations'])
+            del case['citations']
+
+            case['volume_barcode'] = case['volume']['barcode']
+            case['volume_number'] = case['volume']['volume_number']
+            del case['volume']
+
+            case['reporter_name'] = case['reporter']['full_name']
+            case['reporter_id'] = case['reporter']['id']
+            del case['reporter']
+
+            case['court_id'] = case['court']['id']
+            case['court_name'] = case['court']['name']
+            del case['court']
+
+            case['jurisdiction_id'] = case['jurisdiction']['id']
+            case['jurisdiction_name'] = case['jurisdiction']['name_long']
+            del case['jurisdiction']
+
+            del case['preview']
+            del case['casebody']
+            output.writerow(case)
+
